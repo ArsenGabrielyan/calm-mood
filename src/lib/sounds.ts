d57dd01abd9a4895b2b76fecd52da96512ffd270
Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2025-08-09 18:01:29 +0400

    Flipped the key and value pairs

--- src/lib/sounds.ts
+++ src/lib/sounds.ts
@@ -3,22 +3,22 @@ import { ISounds } from "./types"
 const baseUrls = {
      sounds: "https://arsengabrielyan.github.io/calm-mood/sounds",
      icons: "/images/icons"
-}
+} as const
 const volumeProps = {volume: 0}
-const soundsMap: Record<string,string> = {
-     "Թռչուններ": 'birds',
-     "Անձրև": "rain",
-     "Գետ": "river",
-     "Ծովափ": "beach",
-     "Գնացք": "train",
-     "Ամպրոպ": "thunderstorm",
-     "Գիշեր": "night",
-     "Լճափ": "lake",
-}
+const soundsMap = {
+     birds: "Թռչուններ",
+     rain: "Անձրև",
+     river: "Գետ",
+     beach: "Ծովափ",
+     train: "Գնացք",
+     thunderstorm: "Ամպրոպ",
+     night: "Գիշեր",
+     lake: "Լճափ",
+} as const
 
-export const SOUNDS: ISounds[] = Object.entries(soundsMap).map(([key,val])=>({
-     name: key,
-     icon:`${baseUrls.icons}/${val}.svg`,
-     url:`${baseUrls.sounds}/${val}.mp3`,
+export const sounds: ISounds[] = Object.entries(soundsMap).map(([id,name])=>({
+     name,
+     icon:`${baseUrls.icons}/${id}.svg`,
+     url:`${baseUrls.sounds}/${id}.mp3`,
      ...volumeProps
 }))
