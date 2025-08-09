7dbfd502ee2a748f26511d8ac4d565ae7c5063f6
Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2025-08-09 16:41:15 +0400

    Tweaked the exercise page a little

--- src/components/contents/exercise.tsx
+++ src/components/contents/exercise.tsx
@@ -3,38 +3,39 @@ import AppLayout from "@/components/layout";
 import { cn } from "@/lib/utils";
 import { useState, useEffect } from "react";
 
-const EXERCISE_INTERVAL_TIME = 8000;
+const EXERCISE_INTERVAL_TIME = 12500;
 const HOLD_TIME = EXERCISE_INTERVAL_TIME/5;
 const GROW_TIME = HOLD_TIME*2
 
 export default function ExerciseContent(){
      const [text, setText] = useState("Շունչ");
-     const [isGrowing,setIsGrowing] = useState(true);
+     const [circleType, setCircleType] = useState<'growing'|'shrinking'|'hold'>("growing");
      useEffect(() => {
           const startBreathCycle = () => {
                setText("Շունչ");
-               setIsGrowing(true);
+               setCircleType('growing')
                     setTimeout(() => {
                     setText("Պահում");
+                    setCircleType('hold');
                     setTimeout(() => {
                          setText("Արտաշունչ");
-                         setIsGrowing(false);
+                         setCircleType('shrinking');
                     }, HOLD_TIME);
                }, GROW_TIME);
           };
-          startBreathCycle(); // start immediately
+          startBreathCycle();
           const intervalId = setInterval(startBreathCycle, EXERCISE_INTERVAL_TIME);
           return () => clearInterval(intervalId);
      }, []);
      return (
           <AppLayout screenHeight>
                <h1 className="text-3xl md:text-4xl lg:text-5xl font-semibold">Շնչառական վարժություն</h1>
-               <div className="mt-4 space-y-2 flex justify-center items-center">
-                    <div className="bg-card text-card-foreground border shadow-sm rounded-md p-3 flex justify-center items-center flex-col gap-4 max-w-80">
-                         <div className="size-72 flex justify-center items-center rounded-full border-2 border-primary">
-                              <div className={cn("bg-primary rounded-full size-64",isGrowing ? 'animate-grow-circle' : 'animate-shrink-circle')}/>
+               <div className="mt-4 space-y-2 flex justify-center items-center breathing-root">
+                    <div className="bg-card text-card-foreground border shadow-sm rounded-md p-6 flex justify-center items-center flex-col gap-4 max-w-96">
+                         <div className="size-72 flex justify-center items-center rounded-full border-2 border-primary will-change-transform">
+                              <div className={cn("bg-primary rounded-full size-full",circleType==='growing' ? 'animate-grow-circle' : circleType==='hold' ? 'animate-hold-circle' : 'animate-shrink-circle')}/>
                          </div>
-                         <p className="text-xl md:text-2xl lg:text-3xl font-semibold text-primary">{text}</p>
+                         <p className="text-xl md:text-2xl lg:text-3xl font-semibold text-primary" aria-live="polite">{text}</p>
                     </div>
                </div>
           </AppLayout>
