498f0484f4202594696c4408ca2d83dee8e762b6
Arsen Gabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2025-09-11 21:28:05 +0400

    Resized the 404 page

--- src/components/contents/not-found.tsx
+++ src/components/contents/not-found.tsx
@@ -7,7 +7,7 @@ export default function NotFoundContent(){
      return (
           <div className="p-5 flex justify-center items-center flex-col min-h-screen space-y-3">
                <h1 className="text-7xl tracking-tight font-extrabold lg:text-9xl text-primary">404</h1>
-               <p className="text-3xl tracking-tight font-bold md:text-4x">{t("title")}</p>
+               <p className="font-heading text-3xl md:text-4xl lg:text-[42px] tracking-tight font-bold md:text-4x">{t("title")}</p>
                <p className="text-md md:text-lg font-light text-center">{t("description")}</p>
                <Button asChild>
                     <Link href="/">{t("backButton")}</Link>
