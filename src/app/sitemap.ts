678cbfd877c4edbb6f94545ed8634adc4e593828
ArsenGabrielyan <105947988+ArsenGabrielyan@users.noreply.github.com>
2025-11-11 12:17:32 +0400

    Added More Routes to the Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -2,12 +2,26 @@ import { MetadataRoute } from "next";
 import { locales } from "@/i18n/config";
 import { absoluteURL } from "@/lib/utils";
 
-export default function Sitemap(): MetadataRoute.Sitemap{
-     const routes = ["/breathing-exercise","/sounds","/"]
-     return routes.flatMap(route=>locales.map(locale=>({
-          url: absoluteURL(`/${locale}${route === "/" ? "" : route}`),
+function generateSitemapItem(absoluteUrl: string, priority: 1 | 0.8): MetadataRoute.Sitemap[number]{
+     return {
+          url: absoluteURL(absoluteUrl),
           lastModified: new Date(),
           changeFrequency: "weekly",
-          priority: route === "/" ? 1 : 0.8,
-     })))
+          priority
+     }
+}
+
+export default function Sitemap(): MetadataRoute.Sitemap{
+     const routes = [
+          "/",
+          "/breathing-exercise",
+          "/sounds"
+     ]
+     return routes.flatMap(route=>{
+          const priority = route === "/" ? 1 : 0.8;
+          return [
+               generateSitemapItem(`${route === "/" ? "" : route}`,priority),
+               ...locales.map(locale=>generateSitemapItem(`/${locale}${route === "/" ? "" : route}`,priority))
+          ]
+     })
 }
